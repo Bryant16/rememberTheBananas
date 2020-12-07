0e8140fb5daf58063c2b26afb356aa48dfc6c0da
Samantha Butler <samibutler1992@gmail.com>
2020-12-07 01:52:33 -0700

    centered list add modal

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -7,7 +7,10 @@ const confirmAdd = document.querySelector(".confirmAdd");
 
 addList.addEventListener("click", async (event)=>{
     event.preventDefault();
-    modal.style.display = "block";
+    modal.style.display = "inline-block";
+    document.querySelector("#taskfield").style.position = "relative"
+    modal.style.position = "absolute"
+    document.querySelectorAll("main").style.backgroundColor = "black"
 })
 
 close.addEventListener("click", async (event)=> {
@@ -33,8 +36,8 @@ close.addEventListener("click", async (event)=> {
 //     modal.style.display = "none";
 // })
 
-// window.addEventListener("click", async (event)=> {
-//     if(event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// })
+window.addEventListener("click", async (event)=> {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+})
