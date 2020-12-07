87d0d328e22b7ff88827d4e815e1f6cbe4a634e9
nick <biermannick83@gmail.com>
2020-12-06 20:32:13 -0500

    test heroku deployment

--- config/database.js
+++ config/database.js
@@ -11,4 +11,10 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
+
+  production: {
+    use_env_variable: 'postgres://bmvqjykuhbojnn:496bba87547c3adc8473cc45f55076a6afe6893dae4943173af01087293635b8@ec2-54-208-233-243.compute-1.amazonaws.com:5432/d4lo0usn9ef99s',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  }
 };
