6e86b92d5ab23d4826057bdbd109fe6d9cbd24c9
Samantha Butler <samibutler1992@gmail.com>
2020-12-06 19:37:40 -0700

    added use_env_variable key to host on Heroku

--- config/database.js
+++ config/database.js
@@ -11,4 +11,9 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
+  production: {
+    use_env_variable: 'postgres://pyrzlabekulxha:ecbab0b11d09d08652bceb3ef3ea3022f4b37a2ec18f67965b0d8dd1d068fe4d@ec2-52-20-66-171.compute-1.amazonaws.com:5432/d6qpgvq5fbjkb6',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  }
 };
